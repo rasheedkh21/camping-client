fc11fdba3e39799aa3872ab3ea216485b077ba57
Bekzod Abdulkhaev <bekzodabdulkhaev@Bekzods-MacBook-Pro.local>
2023-07-05 21:27:44 +0900

    updated

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -7,36 +7,43 @@ import {
   Loremcontainer,
   Videodiv,
 } from "./style";
+import Popup from "reactjs-popup";
 import Example from "./carouselcomp/carousel";
 import image from "../../assets/car22.png";
 import image1 from "../../assets/car33.png";
 import image2 from "../../assets/imag3car.png";
 import { Overlay } from "./modalpupup";
-import MultiCarousel from "./multiCarousel/MultiCarousel";
-
-
+import MultiCarousel from "./multicarousel/Multicarousel";
 const Home = () => {
-  const [modal, setModal] = useState(false);
+  // const [modal, setModal] = useState(false);
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
 
-  const toggleModal = () => {
-    setModal(!modal);
-  };
-
+  // const toggleModal = () => {
+  //   setModal(!modal);
+  // };
 
   return (
-    <div>
+    <div style={{ width: "100vw" }}>
       <Example />
       <div style={{ display: `flex`, flexDirection: `column` }}>
         <Homecontainer>
           <h1>Recommend</h1>
           <Linediv></Linediv>
-          <MultiCarousel/>
+          <MultiCarousel />
           <h1>Videos</h1>
           <Linediv></Linediv>
           <Videodiv>
-            <video>
-              <source src="https://youtu.be/IP5umfDTiHI" type="video/mp4" />
-            </video>
+            <div
+              onClick={() => setOpen((o) => !o)}
+              style={{ border: "1px solid black" }}
+            >
+              {/* <button>+++</button> */}
+              <video>
+                <source src="https://youtu.be/IP5umfDTiHI" type="video/mp4" />
+              </video>
+            </div>
+
             <video controls>
               <source src="https://youtu.be/K4r6nw6aeg4" type="video/mp4" />
             </video>
@@ -83,6 +90,17 @@ const Home = () => {
           </Blogscontainer>
         </Homecontainer>
       </div>
+      <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+        <div style={{ zIndex: "10" }}>
+          {/* <a className="close" onClick={closeModal}>
+            &times;
+          </a> */}
+
+          <video controls>
+            <source src="https://youtu.be/K4r6nw6aeg4" type="video/mp4" />
+          </video>
+        </div>
+      </Popup>
     </div>
   );
 };
--- src/components/home/multicarousel/multicarousel.jsx
+++ src/components/home/multicarousel/multicarousel.jsx
@@ -1,88 +1,111 @@
-import React from 'react'
-import "slick-carousel/slick/slick.css"; 
+import React from "react";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./MultiCarouselstyle.css";
-import offercar from "../../../assets/offercar1.png"
-import offercar1 from "../../../assets/offercar2.png"
-import offercar2 from "../../../assets/offercar3.png"
-import offercar3 from "../../../assets/offercar4.png"
+import offercar from "../../../assets/offercar1.png";
+import offercar1 from "../../../assets/offercar2.png";
+import offercar2 from "../../../assets/offercar3.png";
+import offercar3 from "../../../assets/offercar4.png";
 
-import Slider from 'react-slick';
+import Slider from "react-slick";
 
 const MultiCarousel = () => {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 4,
-              slidesToScroll: 4,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 834,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 414,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      };
-
-
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          initialSlide: 3,
+        },
+      },
+      {
+        breakpoint: 834,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 414,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
-    <div className='multicarousel'>
-        <Slider {...settings}>
-            <div className="box">
-                <img src={offercar}/>
-                <h1>Cranus</h1>
-                <button>Discover the range</button>
-                <a href="/">Choosa a model</a>
-            </div>
-            <div className="box">
+    <div className="multicarousel">
+      <Slider {...settings}>
+        <div className="box">
+          <div>
+            <img src={offercar} />
+          </div>
+          <h1>Cranus</h1>
+          <div>
+            <button>Discover the range</button>
+          </div>
+          <a href="/">Choosa a model</a>
+        </div>
+        <div className="box">
+          <div>
             <img src={offercar1} />
-            <h1>Aidal</h1>
-                <button>Discover the range</button>
-                <a href="/">Choosa a model</a>
-            </div>
-            <div className="box">
-            <img src={offercar2}/>
-            <h1>Escape</h1>
-                <button>Discover the range</button>
-                <a href="/">Choosa a model</a>
-            </div>
-            <div className="box">
-            <img src={offercar3}/>
-            <h1>Lyseo</h1>
-                <button>Discover the range</button>
-                <a href="/">Choosa a model</a>
-            </div>
-            <div className="box">
-                <img src={offercar}/>
-                <h1>Cranus</h1>
-                <button>Discover the range</button>
-                <p>Choose a model</p>
-            </div>
-        </Slider>
-    </div>    
-  )
-}
+          </div>
+          <h1>Aidal</h1>
+          <div>
+            <button>Discover the range</button>
+          </div>
+          <a href="/">Choosa a model</a>
+        </div>
+        <div className="box">
+          <div>
+            <img src={offercar2} />
+          </div>
+          <h1>Escape</h1>
+          <div>
+            <button>Discover the range</button>
+          </div>
+          <a href="/">Choosa a model</a>
+        </div>
+        <div className="box">
+          <div>
+            <img src={offercar3} />
+          </div>
+          <h1>Lyseo</h1>
+          <div>
+            <button>Discover the range</button>
+          </div>
+
+          <a href="/">Choosa a model</a>
+        </div>
+        <div className="box">
+          <img src={offercar} />
+          <h1>Cranus</h1>
+          <button>Discover the range</button>
+          <p>Choose a model</p>
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
 export default MultiCarousel;
